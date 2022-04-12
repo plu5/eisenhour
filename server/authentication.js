6360020da0e83a46150973b623d533a081379044
plus <ctre@users.sourceforge.net>
2022-04-12 17:02:09 +0300

    Remove old unused passport authentication method

--- server/authentication.js
+++ server/authentication.js
@@ -1,71 +1,12 @@
 const fs = require('fs');
-const express = require('express');
-const session = require('express-session');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-
 const {google} = require('googleapis');
 
 const secrets = require('./secrets');
-const GOOGLE_SCOPES = [
-  'https://www.googleapis.com/auth/calendar.events',
-  // Next ones are needed for passportjs to compute the profile
-  'profile',
-  'email'
-];
+
 
 let access;
 const accessFilePath = 'access.json';
 
-const router = new express.Router();
-router.use(session({
-  secret: secrets.get('EXPRESS_SECRET'),
-  resave: false,
-  saveUninitialized: true,
-}));
-
-passport.use(
-  'google-authentication',
-  new GoogleStrategy(
-    {
-      clientID: secrets.get('GOOGLE_CLIENT_ID'),
-      clientSecret: secrets.get('GOOGLE_CLIENT_SECRET'),
-      callbackURL: 'http://localhost:3003/authentication/google/callback',
-      passReqToCallback: true
-    },
-    // save_token
-    (req, accessToken, refreshToken, profile, done) => {
-      req.session.accessToken = accessToken;
-      req.session.refreshToken = refreshToken;
-      fs.writeFileSync(accessFilePath, JSON.stringify(
-        {access_token: accessToken,
-         refresh_token: refreshToken}), (err) => {
-           if (err) return console.log('Error saving access:', err);
-      });
-      return done(null, profile);
-    }
-  )
-);
-
-router.get('/google',
-  passport.authenticate('google-authentication', {
-    scope: GOOGLE_SCOPES,
-    accessType: 'offline',
-    prompt: 'consent',
-    session: false
-  })
-);
-router.get('/google/callback',
-  passport.authenticate('google-authentication', {
-    failureRedirect: 'http://localhost:3003',
-    session: false
-  }), (req, res) => res.redirect('/authentication/good')
-);
-
-router.get('/good', (req, res) => {
-  const calendar = getCalendar();
-  res.send(calendar);
-});
 
 /**
  * Use existing auth tokens to connect to google and give me a calendar object.
@@ -75,7 +16,6 @@ async function getCalendar() {
   return new Promise((resolve) => {
     const oAuth2Client = new google.auth.OAuth2(
       secrets.get('GOOGLE_CLIENT_ID'), secrets.get('GOOGLE_CLIENT_SECRET'));
-    // oAuth2Client.setCredentials(req.session.accessToken);
     fs.readFile(accessFilePath, (err, content) => {
       if (err) return console.log('Error loading save:', err);
       access = JSON.parse(content);
@@ -96,6 +36,5 @@ async function getCalendar() {
 }
 
 module.exports = {
-  router,
-  getCalendar
+  getCalendar,
 };
--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const authentication = require('./authentication');
 const groups = require('./task-groups');
 const timers = require('./timers');
 const sync = require('./sync');
@@ -15,7 +14,6 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // Routes
-app.use('/authentication', authentication.router);
 app.use('/groups', groups);
 app.use('/timers', timers.router);
 app.use('/sync', sync.router);
