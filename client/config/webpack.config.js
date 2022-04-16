d7e669717b2a760a230e08e10aab04d6d8f95308
plus <ctre@users.sourceforge.net>
2022-04-17 00:44:28 +0300

    Webpack config: Fix modules paths
    Previously was broken when NODE_PATH is not set, because modules does not allow
    having an empty string and will refuse to compile

--- client/config/webpack.config.js
+++ client/config/webpack.config.js
@@ -285,10 +285,10 @@ module.exports = function(webpackEnv) {
       // We placed these paths second because we want `node_modules` to "win"
       // if there are any conflicts. This matches Node resolution mechanism.
       // https://github.com/facebook/create-react-app/issues/253
-      modules: ['node_modules', process.env.NODE_PATH],
-      // ['node_modules', paths.appNodeModules].concat(
-      //  modules.additionalModulePaths || []
-      // ),
+      modules: ['node_modules',
+                paths.appNodeModules,
+               ].concat(modules.additionalModulePaths || [],
+                        process.env.NODE_PATH || []),
       // These are the reasonable defaults supported by the Node ecosystem.
       // We also include JSX as a common component filename extension to support
       // some tools, although we do not recommend using it, see:
