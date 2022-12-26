plu5/eisenhour

commit 5490a8ba1c788b27c567c845fc145ffe1ada1b73

fix(TimerAPI): Prevent same function from subscribing multiple times

client/src/api/TimerAPI.js
@@ -3,10 +3,12 @@ const changeSubscribers = [];
 /**
  * Add a function to changeSubscribers which will be called when a timer data
  *  function is called.
- * @param {Function} callback
+ * @param {Function} f
  */
-function subscribe(callback) {
-  changeSubscribers.push(callback);
+function subscribe(f) {
+  if (changeSubscribers.some((e) =>
+    Object.entries(e).toString() === Object.entries(f).toString())) return;
+  changeSubscribers.push(f);
 }
 
 /**

commit 24d679dfdc30338519fef0b1644b3c8b5f616382

fix(TimerAPI): Override stale subscriber
Bug introduced in 5490a8b

client/src/api/TimerAPI.js
@@ -6,9 +6,13 @@ const changeSubscribers = [];
  * @param {Function} f
  */
 function subscribe(f) {
-  if (changeSubscribers.some((e) =>
-    Object.entries(e).toString() === Object.entries(f).toString())) return;
-  changeSubscribers.push(f);
+  const existing = changeSubscribers.findIndex((e) =>
+    Object.entries(e).toString() === Object.entries(f).toString());
+  if (existing !== -1) {
+    changeSubscribers[existing] = f;
+  } else {
+    changeSubscribers.push(f);
+  }
 }
 
 /**